1484e1daf362ce4624cc6be110ceddf473b84ef8
The Anh <dotheanh12399@gmail.com>
2022-03-22 13:01:14 +0700

    Player rotate to moving direction

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
 
     // Update is called once per frame
     void Update()
@@ -16,6 +18,11 @@ public class Player : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)    // direction.magnitude = length of vector, use to prevent micro move noising
         {
+            // rotate character to target angle
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
             controller.Move(direction * speed * Time.deltaTime);
         }
     }
