aa448eebbcde96fd7ecb6a69b948e240d2fbdc55
The Anh <dotheanh12399@gmail.com>
2022-03-22 12:56:26 +0700

    Move Player by input direction

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CharacterController controller;
+    public float speed = 6f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+        if (direction.magnitude >= 0.1f)    // direction.magnitude = length of vector, use to prevent micro move noising
+        {
+            controller.Move(direction * speed * Time.deltaTime);
+        }
     }
 }
