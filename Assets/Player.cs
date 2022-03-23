1e7a8b95d82383bdde7426a833387ecc45992099
The Anh <dotheanh12399@gmail.com>
2022-03-23 13:17:46 +0700

    Done player movement with seperate graphic and body

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public CharacterController controller;
+    public GameObject PlayerGraphic;
     public float speed = 6f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
@@ -21,8 +22,8 @@ public class Player : MonoBehaviour
         {
             // rotate character to target angle
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + mainCamera.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            float angle = Mathf.SmoothDampAngle(PlayerGraphic.transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            PlayerGraphic.transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDirection.normalized * speed * Time.deltaTime);
