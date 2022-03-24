986f13e88178d6325f157ba59e1dfdb3e9e4e783
The Anh <dotheanh12399@gmail.com>
2022-03-24 13:07:08 +0700

    Spawn item mechanic

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject player;
+    public KeyItem keyItem1, keyItem2, keyItem3;
     // Start is called before the first frame update
     void Start()
     {
         Screen.autorotateToLandscapeLeft = true;
         Screen.orientation = ScreenOrientation.LandscapeLeft;
+
+        // spawn key items to the forest
+        KeyItem[] keyItems = { keyItem1, keyItem2, keyItem3 };
+        foreach(KeyItem keyItem in keyItems)
+        {
+            float min = 230f;
+            float max = 250f;
+            int randomPositiveX = (Random.Range(0f, 1f) > 0.5f) ? 1 : -1;
+            int randomPositiveZ = (Random.Range(0f, 1f) > 0.5f) ? 1 : -1;
+            Vector3 spawnPosition = new Vector3(randomPositiveX*Random.Range(min, max), 
+                                                max, 
+                                                randomPositiveZ*Random.Range(min, max));
+            Debug.Log("spawnPosition " + spawnPosition.x + ", " + spawnPosition.y + ", " + spawnPosition.z);
+            keyItem.SetSpawnPosition(spawnPosition);
+        }
+
     }
 
     // Update is called once per frame
---
+++ Assets/KeyItem.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyItem : MonoBehaviour
+{
+    public Vector3 spawnPosition = new Vector3(0, 0, 0);
+    public float speed = 20;
+
+    private Rigidbody rigi;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void SetSpawnPosition(Vector3 _spawnPosition)
+    {
+        rigi = GetComponent<Rigidbody>();
+        this.spawnPosition = _spawnPosition;
+        rigi.AddForce(this.spawnPosition * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
